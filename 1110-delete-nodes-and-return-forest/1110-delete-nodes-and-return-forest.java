f1529654978adfa1456266aa0ad91fdcb78f63ff
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-10 18:21:09 +0530

    Time: 9 ms (5.08%), Space: 45.6 MB (6.14%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -14,34 +14,54 @@
  * }
  */
 class Solution {
-    Set<Integer> toBeDeletedSet = new HashSet<>();
-    List<TreeNode> solution = new ArrayList<>();
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        for(int i=0;i<to_delete.length;i++)
+        Set<Integer> nodeToBeDeletedSet = new HashSet<>();
+        for(int val: to_delete)
         {
-            toBeDeletedSet.add(to_delete[i]);
+            nodeToBeDeletedSet.add(val);
         }
-        delete(root, toBeDeletedSet);
         
-        if(!toBeDeletedSet.contains(root.val))
-        {
-            solution.add(root);
-        }
-        return solution;
-    }
-    
-    public TreeNode delete(TreeNode root, Set<Integer> toBeDeletedSet)
-    {
-        if(root==null) return null;
-        root.left = delete(root.left, toBeDeletedSet);
-        root.right = delete(root.right, toBeDeletedSet);
+        Queue<TreeNode> queue = new LinkedList<>();
+        Set<TreeNode> solution = new HashSet<>();
         
-        if(toBeDeletedSet.contains(root.val))
+        queue.offer(root);
+        solution.add(root);
+        
+        while(!queue.isEmpty())
         {
-            if(root.left != null) solution.add(root.left);
-            if(root.right != null) solution.add(root.right);
-            return null;
+            TreeNode node = queue.poll();
+            
+            if(nodeToBeDeletedSet.contains(node.val))
+            {
+                solution.remove(node);
+                if(node.left!=null)
+                {
+                    solution.add(node.left);
+                }
+                
+                if(node.right!=null)
+                {
+                    solution.add(node.right);
+                }
+            }
+            if(node.left!=null)
+            {
+                queue.offer(node.left);
+                if(nodeToBeDeletedSet.contains(node.left.val))
+                {
+                    node.left = null;
+                }
+            }
+            if(node.right!=null)
+            {
+                queue.offer(node.right);
+                if(nodeToBeDeletedSet.contains(node.right.val))
+                {
+                    node.right = null;
+                }
+            }
         }
-        return root;
+        return new ArrayList<>(solution);
+        
     }
 }
