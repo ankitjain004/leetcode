dc1c0f235787484226a2850ff1d2dca698688d0c
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-10 18:22:17 +0530

    Time: 3 ms (30.27%), Space: 39.7 MB (60.07%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -65,3 +65,36 @@ class Solution {
         
     }
 }
+
+class RecursiveSolution {
+    Set<Integer> toBeDeletedSet = new HashSet<>();
+    List<TreeNode> solution = new ArrayList<>();
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        for(int i=0;i<to_delete.length;i++)
+        {
+            toBeDeletedSet.add(to_delete[i]);
+        }
+        delete(root, toBeDeletedSet);
+        
+        if(!toBeDeletedSet.contains(root.val))
+        {
+            solution.add(root);
+        }
+        return solution;
+    }
+    
+    public TreeNode delete(TreeNode root, Set<Integer> toBeDeletedSet)
+    {
+        if(root==null) return null;
+        root.left = delete(root.left, toBeDeletedSet);
+        root.right = delete(root.right, toBeDeletedSet);
+        
+        if(toBeDeletedSet.contains(root.val))
+        {
+            if(root.left != null) solution.add(root.left);
+            if(root.right != null) solution.add(root.right);
+            return null;
+        }
+        return root;
+    }
+}
