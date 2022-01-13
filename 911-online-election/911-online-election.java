56c29ab348739019b5c6ba38163e72a38dba2e46
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-13 22:10:22 +0530

    Time: 239 ms (10.54%), Space: 117.3 MB (7.96%) - LeetHub

--- 911-online-election/911-online-election.java
+++ 911-online-election/911-online-election.java
@@ -1,29 +1,28 @@
 class TopVotedCandidate {
     Map<Integer, Integer> map = new TreeMap<>();
     TreeMap<Integer, Integer> timeVsWinnerMap = new TreeMap<>();
+
     public TopVotedCandidate(int[] persons, int[] times) {
         int maxVotes = 0;
         int n = persons.length;
         int winner = -1;
-        
-        for(int i=0;i<n;i++)
-        {
+
+        for (int i = 0; i < n; i++) {
             int person = persons[i];
             int time = times[i];
             int count = map.getOrDefault(person, 0);
-            if(count+1 >= maxVotes)
-            {
+            if (count + 1 >= maxVotes) {
                 winner = person;
-                maxVotes = count+1;
+                maxVotes = count + 1;
                 timeVsWinnerMap.put(time, winner);
             }
-                            
-            map.put(person, count+1);
+
+            map.put(person, count + 1);
         }
     }
-    
+
     public int q(int t) {
-        int key = timeVsWinnerMap.floorKey(t);      
+        int key = timeVsWinnerMap.floorKey(t);
         return timeVsWinnerMap.get(key);
     }
 }
