33f74f431ee001e38ca700c4a2233959cad2240d
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-15 15:04:04 +0530

    Time: 52 ms (61.65%), Space: 41.5 MB (72.57%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+        int maxCount = 0;
+        Map<String, Integer> map = new HashMap<>();
+        int n = s.length();
+        
+        for(int i=0; i<=n-minSize ;i++)
+        {
+            String curr = s.substring(i, i+ minSize);
+            if(eligible(curr, maxLetters))
+            {
+            map.putIfAbsent(curr, 0);
+            int newCount = map.get(curr)+1;
+            maxCount = Integer.max(newCount, maxCount);
+            map.put(curr, newCount);
+            }
+            
+        }
+        
+        return maxCount;
+        
+    }
+    
+    private boolean eligible(String s, int maxLetters)
+    {
+        Set<Character> set = new HashSet<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            set.add(s.charAt(i));
+        }
+        
+        return set.size()<=maxLetters;
+    }
+}
