3722c46b093b17541cbfd9e73103acb58d7e1f68
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-15 15:04:37 +0530

    Time: 209 ms (28.40%), Space: 103 MB (33.50%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.java
@@ -3,33 +3,29 @@ class Solution {
         int maxCount = 0;
         Map<String, Integer> map = new HashMap<>();
         int n = s.length();
-        
-        for(int i=0; i<=n-minSize ;i++)
-        {
-            String curr = s.substring(i, i+ minSize);
-            if(eligible(curr, maxLetters))
-            {
-            map.putIfAbsent(curr, 0);
-            int newCount = map.get(curr)+1;
-            maxCount = Integer.max(newCount, maxCount);
-            map.put(curr, newCount);
+
+        for (int i = 0; i <= n - minSize; i++) {
+            String curr = s.substring(i, i + minSize);
+            if (eligible(curr, maxLetters)) {
+                map.putIfAbsent(curr, 0);
+                int newCount = map.get(curr) + 1;
+                maxCount = Integer.max(newCount, maxCount);
+                map.put(curr, newCount);
             }
-            
+
         }
-        
+
         return maxCount;
-        
+
     }
-    
-    private boolean eligible(String s, int maxLetters)
-    {
+
+    private boolean eligible(String s, int maxLetters) {
         Set<Character> set = new HashSet<>();
-        
-        for(int i=0;i<s.length();i++)
-        {
+
+        for (int i = 0; i < s.length(); i++) {
             set.add(s.charAt(i));
         }
-        
-        return set.size()<=maxLetters;
+
+        return set.size() <= maxLetters;
     }
 }
