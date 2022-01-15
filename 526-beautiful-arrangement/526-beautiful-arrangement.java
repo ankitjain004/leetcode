203dea6fc209d8d76a90a72b5cd48f453e82ff66
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-15 17:38:57 +0530

    Time: 98 ms (28.22%), Space: 37.5 MB (45.68%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.java
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -1,38 +1,34 @@
 class Solution {
     int count = 0;
     int N;
+
     public int countArrangement(int n) {
-        N=n;
-        boolean visited[] = new boolean[n+1];
+        N = n;
+        boolean visited[] = new boolean[n + 1];
         int currCount = 0;
-        
-        for(int i=1;i<=n;i++)
-        {
+
+        for (int i = 1; i <= n; i++) {
             Arrays.fill(visited, false);
             visited[i] = true;
             countUtils(2, visited);
         }
-        
+
         return count;
-        
+
     }
-    
-    
-    public void countUtils(int index, boolean[] visited)
-    {
-        if(index == N+1)
-        {
+
+
+    public void countUtils(int index, boolean[] visited) {
+        if (index == N + 1) {
             count = count + 1;
             return;
         }
-        
-        for(int i=1;i<=N;i++)
-        {
-            if(!visited[i] && (index%i==0 || i%index == 0))
-            {
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && (index % i == 0 || i % index == 0)) {
                 visited[i] = true;
-                countUtils(index+1, visited);
-                visited[i]=false;
+                countUtils(index + 1, visited);
+                visited[i] = false;
             }
         }
     }
