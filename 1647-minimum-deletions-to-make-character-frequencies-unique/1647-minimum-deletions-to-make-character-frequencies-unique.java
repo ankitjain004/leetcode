65189694cc5217dee521c8a62229464430fc1dc1
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-18 02:45:26 +0530

    Time: 26 ms (34.20%), Space: 51.8 MB (35.72%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int minDeletions(String s) {
+        int ans = 0;
+        int[] freq = new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            int index = s.charAt(i)-'a';
+            freq[index] ++;
+        }
+
+        Arrays.sort(freq);
+        int start = 0;
+        int end = 25;
+        while(start<end)
+        {
+            int temp = freq[start];
+            freq[start] = freq[end];
+            freq[end] = temp;
+            start++;
+            end--;
+        }
+
+        int maxVal = freq[0];
+
+        Map<Integer, Integer> treeMap = new TreeMap<>();
+
+        for(int i=0;i<26;i++)
+        {
+            treeMap.put(freq[i], treeMap.getOrDefault(freq[i], 0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=freq[0];i>=1;i--)
+        {
+            if(treeMap.containsKey(i))
+            {
+                int n = treeMap.get(i);
+                if(n>1)
+                {
+                    while(n>1)
+                    {
+                        pq.add(i);
+                        n--;
+                    }
+                }
+            }
+            else
+            {
+                if(!pq.isEmpty())
+                {
+                    int currFreq = pq.poll();
+                    ans = ans + currFreq-i;
+                }
+            }
+        }
+
+
+        while(!pq.isEmpty())
+            ans = ans+pq.poll();
+
+        return ans;
+    }
+
+
+    }
