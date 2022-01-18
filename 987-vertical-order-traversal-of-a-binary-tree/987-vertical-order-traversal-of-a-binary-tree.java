a51d6861472b3dba4e1af9534ae093cbfd9d6632
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-18 18:38:24 +0530

    Time: 5 ms (42.12%), Space: 39.6 MB (40.96%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, List<Pair<Integer, Integer>>> map = new HashMap<>();
+    int minCol = 0;
+    int maxCol = 0;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> sol = new ArrayList<>();
+        recurse(root, 0 , 0);
+        
+        for(int i=minCol; i<=maxCol;i++)
+        {
+            List<Pair<Integer, Integer>> ans = map.get(i);
+            Collections.sort(ans, (a,b )-> {
+                if(a.getKey()!=b.getKey())
+                    return a.getKey()-b.getKey();
+                else
+                {
+                    return a.getValue()-b.getValue();
+                }
+            });
+                List<Integer> temp = new ArrayList<>();
+                for(int j=0;j<ans.size();j++)
+                {
+                    temp.add(ans.get(j).getValue());
+                }
+            sol.add(temp);
+        }
+        
+        return sol;
+        
+    }
+    
+    
+    public void recurse(TreeNode root, int row, int hd)
+    {
+        if(root ==null) return ;
+        
+        
+        map.putIfAbsent(hd, new ArrayList<>());
+        
+        map.get(hd).add(new Pair<Integer, Integer>(row, root.val));
+        minCol = Math.min(minCol, hd);
+        maxCol = Math.max(maxCol, hd);
+        
+        recurse(root.left, row+1, hd-1);
+        recurse(root.right, row+1, hd+1);
+    }
+}
