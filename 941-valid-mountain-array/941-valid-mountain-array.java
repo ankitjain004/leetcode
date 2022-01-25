43277196693b3272cbfe7414135a6f93d1cb01b7
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-25 11:06:20 +0530

    Time: 2 ms (56.28%), Space: 51.9 MB (7.22%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -19,8 +19,8 @@ class Solution {
             }
         }
         
-        if(i==n) return false;
-        if(!isIncreased) return false;
+        if(i==n) return false; // 0 1 2 3 4 5 6 7 8 9
+        if(!isIncreased) return false; //9 8 7 6 5 4 3 2 1 0
         
         while(i<n)
         {
