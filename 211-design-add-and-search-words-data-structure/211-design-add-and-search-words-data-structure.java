d05fb42f7721e3c844839ccc58c9ba52990efca4
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-29 00:48:29 +0530

    Time: 167 ms (5.04%), Space: 74.2 MB (41.61%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,72 @@
+class TrieNode
+{
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean word = false;
+    public TrieNode(){}
+}
+
+
+class WordDictionary {
+    TrieNode node ;
+    public WordDictionary() {
+        node = new TrieNode();
+        
+    }
+    
+    public void addWord(String word) {
+        TrieNode temp = node;
+        for(int  i=0;i<word.length();i++)
+        {
+            Character ch = word.charAt(i);
+            if(temp.children.containsKey(ch))
+            {
+                temp = temp.children.get(ch);
+            }
+            else
+            {
+                temp.children.put(ch, new TrieNode());
+                temp = temp.children.get(ch);
+            }
+        }
+        
+        temp.word = true;
+    }
+    
+    public boolean search(String word)
+    {
+        TrieNode temp = node;
+        return searchUtils(word, temp);
+    }
+    
+    public boolean searchUtils(String word, TrieNode temp) {
+        for(int i=0; i<word.length();i++)
+        {
+            Character ch = word.charAt(i);
+            if(!temp.children.containsKey(ch))
+            {
+                if(ch == '.')
+                 {
+                    for(char curr: temp.children.keySet())
+                    {
+                        TrieNode child = temp.children.get(curr);
+                        if(searchUtils(word.substring(i+1), child))
+                         return true;
+                     }
+                 }
+                  return false;
+                    
+            }
+
+            else
+              temp = temp.children.get(ch);
+        }
+        return temp.word;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
