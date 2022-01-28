ded9a33f343fc231bb1cbcbcbd0079a7dc32ee4b
ankitjain004 <96443169+ankitjain004@users.noreply.github.com>
2022-01-29 01:08:12 +0530

    Time: 52 ms (66.02%), Space: 53.1 MB (51.73%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -1,56 +1,63 @@
-class TrieNode {
+class TrieNode
+{
     Map<Character, TrieNode> children = new HashMap<>();
     boolean word = false;
-
-    public TrieNode() {
-    }
+    public TrieNode(){}
 }
 
 
 class WordDictionary {
-    TrieNode node;
-
+    TrieNode node ;
     public WordDictionary() {
         node = new TrieNode();
-
+        
     }
-
+    
     public void addWord(String word) {
         TrieNode temp = node;
-        for (int i = 0; i < word.length(); i++) {
+        for(int  i=0;i<word.length();i++)
+        {
             Character ch = word.charAt(i);
-            if (temp.children.containsKey(ch)) {
+            if(temp.children.containsKey(ch))
+            {
                 temp = temp.children.get(ch);
-            } else {
+            }
+            else
+            {
                 temp.children.put(ch, new TrieNode());
                 temp = temp.children.get(ch);
             }
         }
-
+        
         temp.word = true;
     }
-
-    public boolean search(String word) {
-        TrieNode temp = node;
-        return searchUtils(word, temp);
+    
+    public boolean search(String word)
+    {
+        return searchUtils(word, node);
     }
-
+    
     public boolean searchUtils(String word, TrieNode temp) {
-        for (int i = 0; i < word.length(); i++) {
+        for(int i=0; i<word.length();i++)
+        {
             Character ch = word.charAt(i);
-            if (temp.children.containsKey(ch)) {
+            if(temp.children.containsKey(ch))
+            {
                 temp = temp.children.get(ch);
-            } else if (ch == '.') {
-                for (char curr : temp.children.keySet()) {
+            }
+            else if(ch == '.')
+            {
+                for(char curr: temp.children.keySet())
+                {
                     TrieNode child = temp.children.get(curr);
-                    if (searchUtils(word.substring(i + 1), child))
+                    if(searchUtils(word.substring(i+1), child))
                         return true;
                 }
-
-                return false;
-
-            } else
-                return false;
+                 return false;
+            }
+            else
+            return false;
+           
         }
         return temp.word;
     }
